a69aa563fbe4e2e483e4bfb9f83f3a7163c203ca
kou <kou.mishima@icloud.com>
2025-09-03 13:45:11 +0900

    commentボタンといいねボタンを横並びに表示

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,10 @@
 class CommentsController < ApplicationController
     def create
-    @comment = Comment.new(comment_params)
-    @comment.user_id = current_user.id
-    if @comment.save
-      redirect_back(fallback_location: root_path)
-    else
-      redirect_back(fallback_location: root_path)
+        @comment = Comment.new(comment_params)
+        @comment.user_id = current_user.id
+        @comment.save
+        redirect_back(fallback_location: root_path)
     end
-
-  end
 
   private
   def comment_params
