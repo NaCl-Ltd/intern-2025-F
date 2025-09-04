3aa0770038a0ba64f5c2eb589d384bd634d25871
Mino-rex <macminosp@icloud.com>
2025-09-04 16:45:22 +0900

    feat: add player and club models

--- app/models/club.rb
+++ app/models/club.rb
@@ -2,4 +2,5 @@ class Club < ApplicationRecord
   has_many :home_matches, class_name: "Match", foreign_key: "home_club_id", dependent: :destroy
   has_many :away_matches, class_name: "Match", foreign_key: "away_club_id", dependent: :destroy
   has_many :forum_threads, dependent: :destroy
+  has_many :players, dependent: :destroy
 end
